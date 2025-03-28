db4116038ac50953089019479e508f8b581ac8ac
Naïl Khelifa <nail.khelifa@ensae.fr>
2025-03-28 12:22:27 +0100

    repo creation 2

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PrototypeProject",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=["pandas", "torch", "numpy",
+                      "matplotlib", "scikit-learn", "h5py",
+                      "tqdm", "seaborn"],
+)
